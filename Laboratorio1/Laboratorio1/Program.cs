8cd7f33d5546cd3b3efc683978ae0819fd49a0e3
Vitor <vitorolimac@gmail.com>
2021-03-14 00:05:32 -0300

    Laboratorio1 resolvido

--- Laboratorio1/Laboratorio1/Program.cs
+++ Laboratorio1/Laboratorio1/Program.cs
@@ -10,6 +10,32 @@ namespace Laboratorio1
     {
         static void Main(string[] args)
         {
+            var usuario1 = new Usuario();
+            usuario1.Nome = "Bob Nelson";
+            usuario1.Idade = 37;
+            usuario1.Altura = 1.70;
+            usuario1.Peso = 78;
+
+            usuario1.CalcularImc();
+            usuario1.CalcularPesoIdeal();
+
+            var usuario2 = new Usuario();
+            usuario2.Nome = "Testolfo Rocha";
+            usuario2.Idade = 43;
+            usuario2.Altura = 1.65;
+            usuario2.Peso = 60;
+
+            usuario2.CalcularImc();
+            usuario2.CalcularPesoIdeal();
+
+            var usuario3 = new Usuario();
+            usuario3.Nome = "Lisa Leite";
+            usuario3.Idade = 22;
+            usuario3.Altura = 1.72;
+            usuario3.Peso = 92;
+
+            usuario3.CalcularImc();
+            usuario3.CalcularPesoIdeal();
         }
     }
 }
---
+++ Laboratorio1/Laboratorio1/Usuario.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1
+{
+    class Usuario
+    {
+        public string Nome;
+        public int Idade;
+        public double Altura;
+        public double Peso;
+
+        double imc;
+        double pesoIdeal;
+        double imcIdeal;
+
+        public void CalcularImc()
+        {
+            imc = Peso / (Altura * Altura);
+            if (imc < 18.5)
+            {
+                Console.WriteLine("{0} está abaixo do peso ideal.",Nome);
+            }
+            else if (imc >= 18.5 && imc <= 24.9)
+            {
+                Console.WriteLine("Parabéns! {0} está em seu peso normal!",Nome);
+            }
+            else if (imc > 24.9 && imc <= 29.9)
+            {
+                Console.Write("{0} está acima de seu peso (sobrepeso).",Nome);
+            }
+            else if (imc > 29.9 && imc <= 34.9)
+            {
+                Console.Write("{0} tem obesidade grau I.", Nome);
+            }
+            else if (imc > 34.9 && imc <= 39.9)
+            {
+                Console.Write("{0} tem obesidade grau II.", Nome);
+            }
+            else if (imc > 39.9)
+            {
+                Console.Write("{0} tem obesidade graus III e IV.", Nome);
+            }
+            Console.ReadLine();
+        }
+        public void CalcularPesoIdeal()
+        {
+            if (imc < 18.5)
+            {
+                imcIdeal = 18.5;
+            }
+            else if(imc>=18.5 && imc <= 24.9)
+            {
+                imcIdeal = imc;
+            }
+            else if (imc > 24.9)
+            {
+                imcIdeal = 24.9;
+            }
+            pesoIdeal = imcIdeal * (Altura * Altura);
+            Console.WriteLine("Seu peso ideal de {0} é {1} Kg.", Nome, pesoIdeal);
+            Console.ReadLine();
+        }
+    }
+}
